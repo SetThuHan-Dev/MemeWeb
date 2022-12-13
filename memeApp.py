86dbda698559f15b033b73258dd53011e2e78bdf
SetThuHan-Dev <courserasth@gmail.com>
2022-12-14 00:13:19 +0630

    Fixed

--- memeApp.py
+++ memeApp.py
@@ -17,4 +17,4 @@ def index():
     meme_pic,subreddit = get_meme()
     return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
 
-app.run(debug=False, host="0.0.0.0")
+app.run()#debug=False, host="0.0.0.0"
