5628217c76731a8fa34b8d2cf69e8cee1170f2d9
SetThuHan-Dev <courserasth@gmail.com>
2022-12-14 01:00:03 +0630

    undo

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template
 import requests
 import json
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 def get_meme():
-    sr = "/wholesomememes"
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
     return meme_large, subreddit
 
-@application.route("/")
+@app.route("/")
 def index():
     meme_pic,subreddit = get_meme()
     return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
 
-application.run(debug=False)
+app.run()
